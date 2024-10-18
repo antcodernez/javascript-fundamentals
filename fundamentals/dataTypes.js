6154dec19735292fb3de89a9294cba937900bcbe
Antonio Jiménez <marshday5@gmail.com>
2024-10-17 22:41:33 -0600

    feat(data-types): implementing the 10 types of datas in js

--- fundamentals/dataTypes.js
+++ fundamentals/dataTypes.js
@@ -2,3 +2,42 @@
 
 // strings
 let name = "Ana Yareli";
+
+// number
+let ageAna = 28;
+
+// boolean
+let hasMayorityAge = true;
+
+// null
+let userdata = null;
+
+// undefined; this type of data are the default javascript let this 
+let catdata = undefined;
+
+// symbol; indicates a unique value 
+let catId = Symbol("Mishifu");
+
+// BigInt
+let number = 2n;
+
+// Complexes
+
+// object
+
+const cats = {}
+cats.genus = "felinidae";
+
+// arrays
+const fruits = ["apple", "orange", "watermelon", "banana"];
+
+
+// functions
+
+function sayHello (name) {
+    console.log(`Hi minino ${name}`);
+}
+
+const hiCat = (name) => {
+    console.log(`Hi minino ${name}`);
+}
