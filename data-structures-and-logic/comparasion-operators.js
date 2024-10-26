389ee27ee4f4da101bc99ff1e8acf0f0e551b631
Antonio Jiménez <marshday5@gmail.com>
2024-10-25 22:45:39 -0600

    feat(comparation-operators): notes

---
+++ data-structures-and-logic/comparasion-operators.js
@@ -0,0 +1,40 @@
+/*
+
+    Comparation operators
+
+*/
+
+// conditional ejecution
+
+// == comparte if something is equal to another thing Example:'2' === 2 true
+
+// === If something is equal to something and compare the data type Example:'2' === 2 flase
+
+//  != Different from value Example: 4 != 3 true 4 != 4 false
+
+//  !== Different from value and data type Example: 4 != 3 true 4 !== 4 false
+
+// > greater than If an value is grater that another one Example: 4 > 2 true 
+
+// < lesser than If an value is lesser that another one Example: 4 < 2 false 
+
+// >= greater and equal than If an value is  greater and equal for one Example: 4 >= 2 true 
+
+// <= lesser and equal than If an value is lesser and equal another one Example: 4 =< 2 false 
+
+const a = 10;
+const b = 20;
+const c = "10";
+
+
+console.log(a == b);
+console.log(a === c);
+console.log(a != c);
+console.log(a !== c);
+console.log(a > b);
+console.log(a < b);
+console.log(a <= c);
+console.log(a >= c);
+
+
+
