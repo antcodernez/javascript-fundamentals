60b048198cfa8d730c05753fbf2254dcc4496d3e
Antonio Jiménez <marshday5@gmail.com>
2024-10-30 08:10:35 -0600

    feat(logic-operators): notes

---
+++ data-structures-and-logic/logic-operators.js
@@ -0,0 +1,17 @@
+/*
+
+    Logic operators
+
+*/
+
+/* console.log(1 && 1);
+console.log(1||2);
+console.log(!null); */
+
+const a = 10;
+const b = 20;
+const c = "10"
+
+console.log(a === b && a ===c);
+console.log(a === b || a === c);
+console.log(!(a === b));
