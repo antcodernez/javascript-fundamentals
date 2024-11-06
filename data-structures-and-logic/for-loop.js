9a0aef37469745af587d617f80bbaf019e23516c
Antonio Jiménez <marshday5@gmail.com>
2024-11-06 07:47:02 -0600

    feat(for): notes about for loop

---
+++ data-structures-and-logic/for-loop.js
@@ -0,0 +1,22 @@
+const fruits = [
+    "Manzana",
+    "Plátano",
+    "Naranja",
+    "Fresa",
+    "Mango",
+    "Uva",
+    "Piña",
+    "Sandía",
+    "Melón",
+    "Cereza",
+    "Papaya",
+    "Durazno",
+    "Kiwi",
+    "Pera",
+    "Frambuesa"
+  ];
+  
+for (let i = 0; i < fruits.length; i++) {
+    const element = fruits[i];
+    console.log(element.toUpperCase());
+}
